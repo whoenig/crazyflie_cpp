d4fcc1037119c684d2acdbdf900ad6b6fbd54070
Wolfgang Hoenig <whoenig@usc.edu>
2016-06-28 22:02:36 -0700

    support for catkin_make and standard cmake

---
+++ CMake.cmake
@@ -0,0 +1,20 @@
+cmake_minimum_required(VERSION 2.8.3)
+project(crazyflie_cpp)
+
+find_library(USB_LIB usb-1.0)
+
+# Enable C++11
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+
+include_directories(
+  include/crazyflie_cpp
+)
+
+add_library(crazyflie_cpp
+  src/Crazyradio.cpp
+  src/Crazyflie.cpp
+)
+
+target_link_libraries(crazyflie_cpp
+  ${USB_LIB}
+)
